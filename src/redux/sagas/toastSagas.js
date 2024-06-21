d98e16aa801dd9b9c38056bacf55c140363d9935
RahulPurbia11 <108067767+RahulPurbia11@users.noreply.github.com>
2024-06-21 15:58:23 +0530

    update delay

--- src/redux/sagas/toastSagas.js
+++ src/redux/sagas/toastSagas.js
@@ -3,7 +3,7 @@ import { takeEvery, put, delay } from "redux-saga/effects";
 import { ADD_TOAST, REMOVE_TOAST } from "../actions/toastActions";
 
 function* handleAddToast(action) {
-  yield delay(30000000000); // Adjust the delay as needed
+  yield delay(3000); // Adjust the delay as needed
   yield put({ type: REMOVE_TOAST, payload: { id: action.payload.id } });
 }
 
